95cb9c1003b400a2bafe4b192d23cffaa05d74e9
Pawan Patle <121163702+PAWANTTR@users.noreply.github.com>
2025-01-01 08:55:35 +0000

    finalcommit

---
+++ .devcontainer/01_Basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score=400
+//console.log(score);
+
+const balance=new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber=23.8699
+//console.log(otherNumber.toPrecision(3));
+
+const hundred=1000000
+//console.log(hundred.toLocaleString('en-IN'));
+
+
+//==========+++++++++++Math++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4)); //negative convert into positive
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4,3,6,8,4));
+// console.log(Math.max(10,5,2,99));
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+
+
